369d5d13f98199843174127a84be5549b69e7136
kdude536 <kiranreji54@gmail.com>
2025-03-10 12:39:29 +0530

    mock test

--- new.py
+++ new.py
@@ -1,3 +1,5 @@
+import time
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 driver=webdriver.Chrome()
@@ -5,5 +7,13 @@ driver.get('https://www.keralapsc.gov.in/answerkey_omrexams')
 lk=driver.find_elements(By.XPATH,"//table[@class='cols-6']/tbody/tr/td[1]/a")
 for i in lk:
     print(i.get_attribute('href'))
-mk=driver.find_element(By.XPATH,"//table[@class='cols-6']/tbody/tr[1]/td[1]/a[1]").click()
-print(mk)
+driver.find_element(By.XPATH,"//table[@class='cols-6']/tbody/tr[2]/td[1]/a[1]").click()
+print(driver.title)
+pdf_url=driver.find_element(By.XPATH,"//div[@class='field field--name-field-omr-question-paper field--type-file field--label-above']//a")
+link_pdf=pdf_url.get_attribute("href")
+print(link_pdf)
+res = requests.get(link_pdf)
+with open("C:\\Users\\Admin\\Desktop\\xy.pdf","wb") as file:
+    file.write(res.content)
+pdf_url.click()
+time.sleep(3)
